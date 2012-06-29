be6a3a354db32f76248bec2539b77d817ce3250f
Diego Medina (fmpwizard) <diego@fmpwizard.com>
2012-06-28 23:55:49 -0400

    We update the page title to the num. of tweets since boot

--- src/main/scala/com/fmpwizard/comet/TweetWritter.scala
+++ src/main/scala/com/fmpwizard/comet/TweetWritter.scala
@@ -3,11 +3,12 @@ package com.fmpwizard.comet
 import net.liftweb.http.NamedCometActorTrait
 import net.liftweb.http.js.jquery.JqJsCmds.PrependHtml
 import xml.{NodeSeq, Text}
-import net.liftweb.http.js.JsCmds.SetHtml
+import net.liftweb.http.js.JsCmds.{Run, Replace, SetHtml}
 import com.twitter.util.Time
 import net.liftweb.json._
 import net.liftweb.json.JsonDSL._
 import net.liftweb.json
+import org.joda.time.DateTime
 
 
 class TweetWritter extends NamedCometActorTrait{
@@ -17,16 +18,19 @@ class TweetWritter extends NamedCometActorTrait{
     case MeanRate(d) => partialUpdate(SetHtml("meanrate", Text(d.toString)))
     case OneMinuteRate(d) => partialUpdate(SetHtml("oneminuterate", Text(d.toString)))
     case FiveMinuteRate(d) => partialUpdate(SetHtml("fiveminuterate", Text(d.toString)))
-    case NumOfTweets(t) => partialUpdate(SetHtml("numoftweets", Text(t.toString)))
+    case NumOfTweets(t) => {
+      partialUpdate(SetHtml("numoftweets", Text(t.toString)))
+      partialUpdate(Run("""$('title').replaceWith('<title>"""+ t.toString+  """ total tweets</title>');""" ))
+    }
   }
 
   def render ={
-    "#lala" #> ""
+    "#boottime" #> (new DateTime()).toString("MMM dd yyyy HH:mm z")
   }
 
   private def formatTweet(t: JValue): NodeSeq ={
     t match {
-      case JNothing => Text("Empty tweet")
+      case JNothing => Text("This tweet was deleted")
       case x        => Text(compact(json.render(x)))
     }
   }
