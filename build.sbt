13b44d43c6fcb4087e136df604818da10a6208f2
Diego Medina (fmpwizard) <diego@fmpwizard.com>
2012-08-08 19:55:36 -0400

    * Working example of using bootstrap with the FlexMenuBuilder trait * Need help getting the divider to work

--- build.sbt
+++ build.sbt
@@ -11,6 +11,10 @@ seq(webSettings :_*)
 resolvers ++= Seq("snapshots" at "http://oss.sonatype.org/content/repositories/snapshots",
                   "releases"  at "http://oss.sonatype.org/content/repositories/releases")
 
+resolvers += "Media4u101 SNAPSHOT Repository" at "http://www.media4u101.se:8081/nexus/content/repositories/snapshots/"
+
+resolvers += "Media4u101 Repository" at "http://www.media4u101.se:8081/nexus/content/repositories/releases/"
+
 libraryDependencies ++= {
 val liftVersion = "2.5-SNAPSHOT"
   Seq(
@@ -18,7 +22,8 @@ val liftVersion = "2.5-SNAPSHOT"
   "org.eclipse.jetty" % "jetty-webapp" % "8.0.1.v20110908" % "container",
   "com.h2database" % "h2" % "1.2.138",
   "org.specs2" %% "specs2" % "1.10" % "test",
-  "ch.qos.logback" % "logback-classic" % "0.9.26"
+  "ch.qos.logback" % "logback-classic" % "0.9.26",
+  "net.liftmodules" %% "fobo" % (liftVersion+"-0.5.2-SNAPSHOT")
   )
 }
 
--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -1,11 +1,15 @@
 package bootstrap.liftweb
 
 import net.liftweb._
+import sitemap.Loc._
 import util._
 
 import common._
 import http._
 import sitemap._
+
+import net.liftmodules.FoBo
+import xml.NodeSeq
 
 
 /**
@@ -20,14 +24,10 @@ class Boot {
     // where to search snippet
     LiftRules.addToPackages("com.fmpwizard")
 
-    // Build SiteMap
-    val entries = List(
-      Menu.i("Index") / "index"
-    )
 
     // set the sitemap.  Note if you don't want access control for
     // each page, just comment this line out.
-    LiftRules.setSiteMap(SiteMap(entries:_*))
+    LiftRules.setSiteMap(Paths.sitemap)
 
 
     //Show the spinny image when an Ajax call starts
@@ -44,7 +44,33 @@ class Boot {
     // set DocType to HTML5
     LiftRules.htmlProperties.default.set((r: Req) =>new Html5Properties(r.userAgent))
 
+    FoBo.InitParam.ToolKit=FoBo.Bootstrap204
+    FoBo.init()
+
   } //boot
 
 } //Boot
 
+object Paths {
+
+  val index = Menu.i("Index") / "index"
+  val page2 = Menu.i("page2") / "index2"
+  val page3 = Menu.i("page3") / "index3"
+  val page4 = Menu.i("page4") / "index4"
+  val page5 = Menu.i("page5") / "index5"
+
+  def sitemap = SiteMap(
+    index >> Snippet("mysnippet", addDivider _ )  submenus(
+      page2,
+      page3
+    ),
+    page4,
+    page5
+  )
+
+  def addDivider( ns: NodeSeq): NodeSeq ={
+    println("we got " + ns)
+    ns
+  }
+
+}
