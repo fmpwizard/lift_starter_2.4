00b2b11d73c6ca5bd89f9e3602b837fce6c72d9b
Diego Medina (fmpwizard) <diego@fmpwizard.com>
2013-01-06 23:11:35 -0500

    * Added more logging * Solved a bug where each new comet would toggle the pin status. * Added CometManager, so all sessions see the current status of each pin * Tested Pin 1 to make sure it really toggles based on the UI

--- src/main/scala/com/fmpwizard/comet/Gpio.scala
+++ src/main/scala/com/fmpwizard/comet/Gpio.scala
@@ -39,7 +39,7 @@ class Gpio extends CometActor with Loggable with CometListener {
     case PinUp(pin)     => pin.high()
     case PinDown(pin)   => pin.low()
     case PinToggle(pin) =>
-      pin.toggle()
+      logger.info("1- pin status: " + pin.getState)
       partialUpdate(JE.JsRaw("""$("#%s").html("pin status: %s")""".format(pin.getName, pin.isHigh)).cmd)
   }
 
@@ -48,6 +48,8 @@ class Gpio extends CometActor with Loggable with CometListener {
    */
   private[this] def togglePin(pin: JValue): JsCmd = {
     GpioCometManager ! PinToggle(pin)
+    val p: GpioPinDigitalOutput = pin
+    logger.info("toogle pin " + p)
     JsCmds.Noop
   }
 
@@ -57,8 +59,8 @@ class Gpio extends CometActor with Loggable with CometListener {
    */
   private[this] implicit def jv2Pin(in: JValue): GpioPinDigitalOutput = {
     implicit val foprmats = DefaultFormats
-    logger.debug("in is %s" format in)
-    logger.debug("pin is %s" format ((in \ "pin" ).extract[String]))
+    logger.info("in is %s" format in)
+    logger.info("pin is %s" format ((in \ "pin" ).extract[String]))
     (in \ "pin").extract[String] match {
       case "pin1" => pin1
       case "pin2" => pin2
@@ -70,6 +72,12 @@ class Gpio extends CometActor with Loggable with CometListener {
       case "pin8" => pin8
       case "pin9" => pin9
       case "pin10" => pin10
+      case "pin11" => pin11
+      case "pin12" => pin12
+      case "pin13" => pin13
+      case "pin14" => pin14
+      case "pin15" => pin15
+      case "pin16" => pin16
       case _ => println("ee") ; pin1
     }
   }
--- src/main/scala/com/fmpwizard/comet/GpioCometManager.scala
+++ src/main/scala/com/fmpwizard/comet/GpioCometManager.scala
@@ -4,18 +4,23 @@ import net.liftweb.actor.LiftActor
 import net.liftweb.http.ListenerManager
 import com.fmpwizard.PinToggle
 import com.fmpwizard.gpio.Controller
+import net.liftweb.common.Loggable
 
 /**
  * This LiftActor tells all comet actors in this jvm when a pin state has changed.
  */
-object GpioCometManager extends LiftActor with ListenerManager {
+object GpioCometManager extends LiftActor with ListenerManager with Loggable {
+
   var pin: PinToggle = PinToggle(Controller.pin1)
 
   def createUpdate = pin
   override def lowPriority = {
     case m@PinToggle(currentPi) =>
+      currentPi.toggle()
       pin = m
+      logger.info("3- pin is: " + pin)
       updateListeners()
+      logger.info("4- pin is: " + pin)
   }
 }
 
