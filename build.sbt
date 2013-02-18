a3a2e4c36754c1e52dd53389d75c65c5bd0bf3c7
Diego Medina (fmpwizard) <diego@fmpwizard.com>
2013-02-17 21:32:30 -0500

    wip

--- build.sbt
+++ build.sbt
@@ -18,7 +18,6 @@ val liftVersion = "2.5-M4"
   "net.liftmodules"   %% "fobo"           % (liftVersion+"-0.8.0-SNAPSHOT"),
   "org.specs2"        %% "specs2"         % "1.10"            % "test",
   "org.eclipse.jetty" % "jetty-webapp"    % "8.0.1.v20110908" % "container",
-  "com.h2database"    % "h2"              % "1.2.138",
   "ch.qos.logback"    % "logback-classic" % "0.9.26"
   )
 }
--- src/main/scala/com/fmpwizard/snippet/Sample.scala
+++ src/main/scala/com/fmpwizard/snippet/Sample.scala
@@ -1,16 +1,52 @@
 package com.fmpwizard.snippet
 
-import net.liftweb.http.SHtml
+import net.liftweb.http._
 import net.liftweb.http.js.{JE, JsCmds, JsCmd}
 import net.liftweb.util.Helpers._
+import net.liftweb.http.js.JsCmds.{Script, OnLoad}
+import net.liftweb.actor.LAFuture
+import net.liftweb.http.rest.RestContinuation
+import net.liftweb.common.Full
+import net.liftweb.common.Full
 
 class Sample{
-  def render = {
-    "#button2 [onclick]" #> SHtml.ajaxCall( JE.JsRaw("this.name"),  handleClick _ )
+
+  val f1: LAFuture[String] = new LAFuture()
+
+  /**
+   * Imagine this is a call to a 3rd party service that takes a long time.
+   */
+  def showFuture: JsCmd = {
+
+    val req = S.request
+    if ( req.map(_.request.suspendResumeSupport_?) == Full( true ) ) {
+      //S.request.map(_.request.suspend(LiftRules.cometGetTimeout))
+      S.request.map(_.request.suspend(3000L))
+      //This is our super computation, goes off and takes a huge amount of time, a full 3 seconds.
+      //Thread.sleep(3000L)
+      f1.satisfy(Thread.currentThread().getName)
+      val js = JE.JsRaw("""$("#result-from-external-service").text("This task run on the thread name: %s")""".format(f1.get(4000L))).cmd &
+        JE.JsRaw("""$("#result-from-external-service").attr("class", "alert alert-success")""").cmd
+
+      //S.request.map(_.request.resume(req.openOr(Req.nil), JavaScriptResponse( js ) ))
+      JsCmds.Noop
+      js
+    } else {
+      //We just give up if the container does not support continuation.
+      JsCmds.Noop
+    }
   }
 
-  def handleClick(s: String): JsCmd = {
-    JE.JsRaw("""alert("Button clicked, value was: %s")""".format(s)).cmd
+  /*def showFuture: JsCmd = {
+    Thread.sleep(3000L)
+    f1.satisfy(Thread.currentThread().getName)
+    JE.JsRaw("""$("#result-from-external-service").text("This task run on the thread name: %s")""".format(f1.get(4000L))).cmd &
+    JE.JsRaw("""$("#result-from-external-service").attr("class", "alert alert-success")""").cmd
+  }*/
+
+  def render = {
+    "#future"           #> Script(OnLoad(SHtml.ajaxInvoke(() => showFuture).exp.cmd) ) &
+    "#render-thread *"  #> Thread.currentThread().getName
   }
 
 
