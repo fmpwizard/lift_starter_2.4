188bfa55fd46257c45ff16b4f1b0753fa51b3f66
Diego Medina (fmpwizard) <diego@fmpwizard.com>
2013-02-28 23:38:09 -0500

    * Separarted js into two files, to keep it simple * fixed some minor bugs in js and templates

--- src/main/webapp/static/js/trigger-js-event.js
+++ src/main/webapp/static/js/trigger-js-event.js
@@ -2,9 +2,6 @@
   $(document).ready(function() {
     //Just like Lift removes elements with this class name
     $('.clearable').remove();
-    var chatMessages = new ChatMessages();
-
-    //
     $(document).on('new-chat-message', function(event, data) {
       $('#messages').append('<li>' + data + '</li>')
     });
