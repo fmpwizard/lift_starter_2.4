dad5555c2c5ac9d75889321d1b494d1925e94246
Diego Medina (fmpwizard) <diego@fmpwizard.com>
2013-03-20 15:50:33 -0400

    * Show that adding addPostPageJavaScript from fixedRender adds the javascript twice

--- src/main/scala/code/comet/ChatKnockOutJs.scala
+++ src/main/scala/code/comet/ChatKnockOutJs.scala
@@ -4,7 +4,7 @@ package comet
 import net.liftweb._
 import common.Loggable
 import http._
-import js.{JE, JsCmd}
+import js.{JsCmds, JE, JsCmd}
 import json._
 import json.JsonDSL._
 import util._
@@ -17,31 +17,11 @@ import xml.NodeSeq
  * by this component.  When the component changes on the server
  * the changes are automatically reflected in the browser.
  */
-class ChatKnockOutJs extends CometActor with CometListener with Loggable {
-  private var msgs: Vector[String] = Vector() // private state
+class ChatKnockOutJs extends CometActor with Loggable {
 
-  /**
-   * When the component is instantiated, register as
-   * a listener with the ChatServer
-   */
-  def registerWith = ChatServer
-
-  /**
-   * The CometActor is an Actor, so it processes messages.
-   * In this case, we're listening for Vector[String],
-   * and when we get one, update our private state
-   * and reRender() the component.  reRender() will
-   * cause changes to be sent to the browser.
-   */
   override def lowPriority = {
-    case v: Vector[String] =>
-      msgs = v
-      partialUpdate(NewMessageKo(v.last))
-
-    case InitialRender =>
-      partialUpdate(InitialMessages( msgs ))
+    case s => s
   }
-
   /**
    * Clear any elements that have the clearable class.
    */
@@ -50,23 +30,10 @@ class ChatKnockOutJs extends CometActor with CometListener with Loggable {
   }
 
   override def fixedRender = {
-    this ! InitialRender
+    S.session map { sess =>
+      sess.addPostPageJavaScript( () => JsCmds.Alert("hi") )
+    }
     NodeSeq.Empty
   }
 }
 
-case class NewMessageKo(message: String) extends JsCmd {
-  implicit val formats = DefaultFormats.lossless
-  val json: JValue = ("message" -> message)
-  override val toJsCmd = JE.JsRaw(""" $(document).trigger('new-ko-chat', %s)""".format( compact( render( json ) ) ) ).toJsCmd
-}
-
-case class InitialMessages(messages: Vector[String]) extends JsCmd {
-  implicit val formats = DefaultFormats.lossless
-  val json: JValue = messages.map{ m =>
-    ("message" -> m)
-  }
-  override val toJsCmd = JE.JsRaw(""" $(document).trigger('initial-chat-messages', %s)""".format( compact( render( json ) ) ) ).toJsCmd
-}
-
-case object InitialRender
